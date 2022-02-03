c5cb4448341e5683290eb7689b0ac7c82ac8dc9d
Kase Lunt <kllunt@asu.edu>
2022-02-03 16:21:36 -0700

    python script for interacting with ganache

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,5 +1,12 @@
+from cmath import pi
+from itertools import chain
 from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -32,4 +39,38 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
-print(abi)
+# for connecting to Ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x1AF6064d98380aCbA655fAb9A00186Fe4170afBF"
+private_key = os.getenv("PRIVATE_KEY")
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# Send this signed transaction
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+# Working with a contract, you always need:
+# Contract address
+# Contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+print(simple_storage.functions.retrieve())
