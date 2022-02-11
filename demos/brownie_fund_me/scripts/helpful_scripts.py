db3f6c93ad1281ecafc2aed8b7cf94b2ecc7f76c
Kase Lunt <kllunt@asu.edu>
2022-02-10 22:14:48 -0700

    finished deployment script

--- demos/brownie_fund_me/scripts/helpful_scripts.py
+++ demos/brownie_fund_me/scripts/helpful_scripts.py
@@ -1,14 +1,18 @@
 from brownie import network, config, accounts, MockV3Aggregator
 from web3 import Web3
 
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local-1"]
 
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 20000000000000
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
@@ -19,6 +23,9 @@ def deploy_mocks():
     print(f"Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
         MockV3Aggregator.deploy(
-            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+            # DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+            DECIMALS,
+            STARTING_PRICE,
+            {"from": get_account()},
         )
     print("Mocks Deployed!")
